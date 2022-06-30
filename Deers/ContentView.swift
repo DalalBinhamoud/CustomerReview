fd722479fa5e695c98f574aad104e5c5a304a005
Rating <dalalbinhamoud@gmail.com>
2022-06-30 13:18:39 +0300

    implement  review page design

--- Deers/ContentView.swift
+++ Deers/ContentView.swift
@@ -9,13 +9,71 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!!")
-            .padding()
+
+        ZStack{
+            Image("background").resizable().scaledToFit().opacity(0.25)
+
+            VStack{
+                Spacer()
+                Text("How was your experience?").font(.custom("riesling", size: 60))
+
+                HStack{
+                    CustomButton(
+                               status: "bad",
+                               icon: Image("bad")
+                           ) {
+                               print("bad Clicked!")
+                           }
+   
+                    CustomButton(
+                               status: "ok",
+                               icon: Image("ok")
+                           ) {
+                               print("ok Clicked!")
+                           }
+                    
+                    CustomButton(
+                               status: "good",
+                               icon: Image("good")
+                           ) {
+                               print("good Clicked!")
+                           }
+                   
+                }
+                Spacer()
+                
+                VStack{
+                    Divider()
+                    HStack{
+                        Image("globe").resizable().frame(width: 32.0, height: 32.0)
+                        Text("example.com").font(.custom("riesling", size: 30))
+                    }
+                    
+                    HStack{
+                        Image("mail").resizable().frame(width: 32.0, height: 32.0)
+                        Text("example.com").font(.custom("riesling", size: 30))
+                    }
+                    HStack{
+                
+                    Image("instagram").resizable().frame(width: 32.0, height: 32.0)
+                    Text("example.com").font(.custom("riesling", size: 30))
+                }
+                    HStack{
+                Image("twitter").resizable().frame(width: 32.0, height: 32.0)
+                Text("example.com").font(.custom("riesling", size: 30))
+            }
+                }
+                
+            
+            }
+        }
+     
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.landscapeRight)
     }
 }
