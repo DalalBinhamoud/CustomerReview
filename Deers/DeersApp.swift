6d47df7cd18261512fa36906f3c163545290699b
Rating <dalalbinhamoud@gmail.com>
2022-06-30 11:01:00 +0300

    initial commit

---
+++ Deers/DeersApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeersApp.swift
+//  Deers
+//
+//  Created by Dalal Macbook on 30/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct DeersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
